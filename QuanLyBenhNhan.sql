20e53040ec2f62c9936953e85949b60f64ad8a7a
DTH235812-NHoangUy <uy_dth235812@student.agu.edu.vn>
2025-10-29 15:40:51 +0700

    git len

--- QuanLyBenhNhan.sql
+++ QuanLyBenhNhan.sql
@@ -5,7 +5,7 @@ DROP DATABASE IF EXISTS quanlybenhnhan;
 CREATE DATABASE quanlybenhnhan;
 USE quanlybenhnhan;
 
--- Tạo bảng benhnhan
+
 CREATE TABLE benhnhan (
     MaBN VARCHAR(10) PRIMARY KEY,
     HoBN VARCHAR(50),
@@ -13,10 +13,50 @@ CREATE TABLE benhnhan (
     GioiTinh VARCHAR(10),
     TuoiBN INT,
     SDTBN INT,
-    NgaySinh DATETIME,
+    NgaySinh DATE,
     DiaChi VARCHAR(100)
 );
-CREATE TABLE macbenh(
-	MaBenh VARCHAR(10) PRIMARY KEY,
-    LoaiBenh VARCHAR(50)
+
+CREATE TABLE macbenh (
+    MaBenh VARCHAR(10) PRIMARY KEY,
+    LoaiBenh VARCHAR(10)
+);
+CREATE TABLE dieutri (
+    NgayBDDieuTri DATE,
+    NgayXuatVien DATE
+);
+CREATE TABLE BHYT (
+    MaBHYT VARCHAR(20) PRIMARY KEY,
+    NgayBDBHYT DATE,
+    NgayHHBHYT DATE,
+    QueQuan VARCHAR(100)
+);
+CREATE TABLE Khoa (
+    MaKhoa VARCHAR(10) PRIMARY KEY,
+    TenKhoa VARCHAR(100)
+);
+CREATE TABLE ChucVu (
+    MaCV VARCHAR(10) PRIMARY KEY,
+    TenCV VARCHAR(100)
+);
+CREATE TABLE NhanVien (
+    MaNV VARCHAR(10) PRIMARY KEY,
+    HoTenNV VARCHAR(100),
+    TuoiNV INT,
+    GioiTinhNV VARCHAR(10),
+    SDTNV VARCHAR(15),
+    MaKhoa VARCHAR(10),
+    MaCV VARCHAR(10),
+    FOREIGN KEY (MaKhoa) REFERENCES Khoa(MaKhoa),
+    FOREIGN KEY (MaCV) REFERENCES ChucVu(MaCV)
+);
+CREATE TABLE DonThuoc (
+    MaDT VARCHAR(10) PRIMARY KEY,
+    TenDT VARCHAR(100),
+    CachSuDung VARCHAR(200),
+    DonViTinh VARCHAR(50)
+);
+CREATE TABLE Thuoc (
+    MaThuoc VARCHAR(10) PRIMARY KEY,
+    TenThuoc VARCHAR(100)
 );
